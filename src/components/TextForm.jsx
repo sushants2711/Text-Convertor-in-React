9b9a0971150f929e9eb92058d181461fddf5befd
sushantkumarsingh <singhji1359@gmail.com>
2024-11-27 16:18:51 +0530

    new functions added

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -17,19 +17,44 @@ export default function TextForm(props) {
         setText(text.toLowerCase());
     }
 
+
+    const handleReset = () => {
+        setText("")
+    }
+
+    const handleCapitalize = () => {
+        setText(
+            text
+                .split(" ") 
+                .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) 
+                .join(" ") 
+        );
+    };
+    
+
     return (
         <>
             <div className="container mt-4">
                 <h1 className="mt-4 mb-4">{props.headingis}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control bg-dark text-white" id="exampleFormControlTextarea1" rows="14" onChange={handleOnChange} value={text}
+                    <textarea className="form-control bg-dark text-white" id="exampleFormControlTextarea1" rows="10" onChange={handleOnChange} value={text}
                     ></textarea>
                 </div>
+                <Button text="UPPERCASE" onClick={handleUppercase} className="btn btn-primary mx-4" />
+                <Button text="lowercase" onClick={handlelowercase} className="btn btn-primary mx-4" />
+                <Button text="Reset" onClick={handleReset} className="btn btn-danger mx-4" />
+                <Button text="Capitalize Case" onClick={handleCapitalize} className='btn btn-primary mx-4'/>
             </div>
 
-            <Button text="UPPERCASE" onClick={handleUppercase} className="btn btn-primary mx-4" />
-            <Button text="lowercase" onClick={handlelowercase} className="btn btn-primary mx-4" />
-        </>
+            <div className="container mt-4">
+                <h4> Your Text Summary </h4>
+                <p> Word is: <b> {! text.trim().length ? 0 : text.trim().split(" ").length} </b> & characters is: <b> {text.trim().length} </b> </p>
+            </div>
+
+            <div className="container">
+                <p><b>Preview: </b> {text} </p>
+            </div>
+        </> 
     );
 }
 
